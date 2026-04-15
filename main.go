f8216acf6f099b17a5b84833167b6fce159f883a
Qaid-Danial <muhammadqaidd@gmail.com>
2026-04-15 20:58:10 +0800

    point: checkpoint 1

--- main.go
+++ main.go
@@ -3,39 +3,84 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
+
+type Status string
+
+const (
+	toDo Status = "to-do"
+	inProgress Status = "in-progress"
+	done Status = "done"
+)
+
+type tasks struct {
+
+	id int
+	description string 
+	status Status
+	createdAt time.Time
+	updatedAt time.Time
+
+}
 
 func main() {
 
-	var command string
+	// var command string 
+	// var description string
+	// var id int
 
-	for {		
+	var command1, command2 string
 
-		fmt.Print("Task-CLI ")
-		n, err := fmt.Scanln(&command)
-		
-		command = strings.ToLower(command)
+	fmt.Print("> ")
 
-		if n != 0 && err == nil {
-			
-			switch command {
-			case "exit", "e":
-				fmt.Println("Exiting")
-				return
+	command1 = strings.ToLower(command1)
+	command2 = strings.ToLower(command2)
 
-			case "add", "a":
-				fmt.Println("Adding Task")
+	fmt.Scanf("%s", &command1)
 
-			case "update", "u":
-				fmt.Println("Updating Task")
-
-			case "delete", "d":
-				fmt.Println("Deleting Task")
-
-			default:
-				fmt.Println("Invalid")
-			}
-		} 
+	if command1 == "pipi" {
+		fmt.Scanf("%q", &command2)
+	} else if command1 == "popo" {
+		fmt.Scanf("%s", &command2)
 	}
+
+
+	fmt.Println(command1)
+	fmt.Println(command2)
+	
+
+
+	// for {		
+
+	// 	fmt.Print("Task-CLI ")
+	// 	n, err := fmt.Scanln(&command)
+		
+	// 	command = strings.ToLower(command)
+
+	// 	if n != 0 && err == nil {
+			
+	// 		switch command {
+	// 		case "exit", "e":
+	// 			fmt.Printf("\nExiting\n\n")
+	// 			return
+
+	// 		case "add", "a":
+	// 			fmt.Printf("\nAdding Task\n\n")
+
+	// 		case "update", "u":
+	// 			fmt.Printf("\nUpdating Task\n\n")
+				
+
+	// 		case "delete", "d":
+	// 			fmt.Printf("\nDeleting Task\n\n")
+
+
+	// 		default:
+	// 			fmt.Printf("\nInvalid\n\n")
+
+	// 		}
+	// 	} 
+	// }
 
 }
